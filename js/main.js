89f0456096e16f2ca6f667f0a4e7741adc2423b0
nickyvanurk <nickyvanurk@gmx.com>
2017-09-29 11:20:16 +0200

    Delete old code

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ let previous = performance.now();
 let lag = 0;
 
 function gameLoop(time) {
-  let current = performance.now();
-  let elapsed = current - previous;
+  const current = performance.now();
+  const elapsed = current - previous;
   previous = current;
   lag += elapsed;
 
@@ -26,16 +26,3 @@ function gameLoop(time) {
 }
 
 requestAnimationFrame(gameLoop);
-
-
-// let game = new Game();
-// let timePerFrameInSeconds = 1 / 60;
-// let timePerFrameInMilliseconds = 1000 / 60;
-
-// window.addEventListener('keydown', game.processEvents.bind(game));
-// window.addEventListener('keyup', game.processEvents.bind(game));
-
-// setInterval(() => {
-//   game.update(timePerFrameInSeconds);
-//   game.draw();
-// }, timePerFrameInMilliseconds);
